1b07e061fbe1f98e8dbfca2fca79d8cacef4bb1f
Jeon Minji <mjjeon2645@gmail.com>
2023-06-22 20:53:33 +0900

    markdown 라이브러리 설치 후 md 파일 생성하여 샘플 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
+import { graphql } from 'gatsby';
+
 import Introduction from '../components/main/Introduction';
 import Footer from '../components/common/Footer';
 import Layout from '../components/layout';
 import CategoryList from '../components/main/CategoryList';
 import PostList from '../components/main/PostList';
+
+import { PostListItemType } from '../types';
 
 const Container = styled.div`
   display: flex;
@@ -19,15 +23,52 @@ const CATEGORY_LIST = {
   News: 3,
 };
 
-export default function IndexPage() {
+type IndexPageProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[];
+    }
+  }
+}
+
+export default function IndexPage({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}: IndexPageProps) {
   return (
     <Layout>
       <Container>
         <Introduction />
         <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-        <PostList />
+        <PostList posts={edges} />
         <Footer />
       </Container>
     </Layout>
   );
 }
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
